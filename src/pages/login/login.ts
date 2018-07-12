b3ea34bc7f6a39bfb5e536c9c897371e862946f8
Coskun Cinar <cinarcoskun@gmail.com>
2018-07-13 02:44:38 +0300

    v-0.0.6 hazırlık. Login mesaj düzeltildi.
    Signed-off-by: Coskun Cinar <cinarcoskun@gmail.com>

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { NavController, AlertController, LoadingController, Loading, IonicPage } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
 import { TabsPage } from '../tabs/tabs';
- 
 
-interface ISonuc   {
+
+interface ISonuc {
   readonly result: any;
-  readonly message:any;
+  readonly message: any;
   readonly data: any;
 }
 
@@ -15,51 +15,55 @@ interface ISonuc   {
   selector: 'page-login',
   templateUrl: 'login.html',
 })
- 
+
 
 export class LoginPage {
   loading: Loading;
   registerCredentials = { email: '', password: '' };
-  public check : boolean = true;
+  public check: boolean = true;
 
   constructor(
-      private nav: NavController, 
-      private auth: AuthProvider, 
-      private alertCtrl: AlertController, 
-      private loadingCtrl: LoadingController ) {}
-  
+    private nav: NavController,
+    private auth: AuthProvider,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) { }
+
 
   ionViewDidEnter() {
-   
+
   }
-  
+
   public login() {
-    this.showLoading() 
+    this.showLoading()
     this.auth.login(this.registerCredentials).
-      then((sonuc:ISonuc) => { 
-         if (sonuc.result ==="success") {        
+      then((sonuc: ISonuc) => {
+        if (sonuc.result === "success") {
           this.nav.setRoot(TabsPage);
         } else {
-          this.showError(sonuc.message);
-        }         
-    }, (err) => {
-      this.showError(err);
-    }); 
+          this.showError("Giriş hatası!",sonuc.message);
+        }
+      }, (err) => {
+        //console.log(err);
+          console.log(err.statusText);
+        if (err.statusText == "Unknown Error") {
+          this.showError("Erişim hatası!","Sizinproje.com servislerine erişilemiyor!");
+        }
+      });
   }
- 
+
   showLoading() {
     this.loading = this.loadingCtrl.create({
       content: 'Lütfen bekleyiniz...',
-      dismissOnPageChange: true, 
+      dismissOnPageChange: true,
     });
     this.loading.present();
   }
- 
-  showError(text) {
-    this.loading.dismiss(); 
+
+  showError(caption,text) {
+    this.loading.dismiss();
     let alert = this.alertCtrl.create({
-      title: 'Hata',
-      subTitle: text, 
+      title: caption,
+      subTitle: text,
       buttons: ['Tamam']
     });
     alert.present();
